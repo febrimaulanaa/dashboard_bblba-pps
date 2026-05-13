29ca571dfd703722b3cadaf0cac04e284162ef8e
Febri <febrimaulanaa@gmail.com>
2026-05-13 14:09:23 +0700

    Fix production config - APP_URL and SESSION_DOMAIN for CSRF

--- app/Http/Controllers/AbsensiPegawaiController.php
+++ app/Http/Controllers/AbsensiPegawaiController.php
@@ -25,7 +25,7 @@ class AbsensiPegawaiController extends Controller
         }
     }
 
-    public function store(Request $request)
+public function store(Request $request)
     {
         $request->validate([
             'jenis_tutorial' => 'required',
@@ -58,18 +58,80 @@ class AbsensiPegawaiController extends Controller
             'longitude' => 'required|numeric',
         ]);
 
-        $fileMateri = null;
-        if ($request->hasFile('file_materi')) {
-            $file = $request->file('file_materi');
-            $filename = time() . '_materi.' . $file->getClientOriginalExtension();
-            
-            \Illuminate\Support\Facades\Storage::disk('google')->put($filename, file_get_contents($file));
-            
-            $adapter = \Illuminate\Support\Facades\Storage::disk('google')->getAdapter();
-            $metadata = $adapter->getMetadata($filename);
-            // $metadata['path'] is the file ID in Google Drive
-            $fileMateri = 'https://drive.google.com/uc?id=' . $metadata['path'];
+        try {
+            $fileMateri = null;
+            $filePeserta = null;
+
+            if ($request->hasFile('file_materi')) {
+                try {
+                    $file = $request->file('file_materi');
+                    $filename = time() . '_materi.' . $file->getClientOriginalExtension();
+                    
+                    if (\Illuminate\Support\Facades\Storage::disk('google')->put($filename, file_get_contents($file))) {
+                        $adapter = \Illuminate\Support\Facades\Storage::disk('google')->getAdapter();
+                        $metadata = $adapter->getMetadata($filename);
+                        $fileMateri = 'https://drive.google.com/uc?id=' . $metadata['path'];
+                    }
+                } catch (\Exception $e) {
+                    \Illuminate\Support\Facades\Log::error('Upload file materi error: ' . $e->getMessage());
+                }
+            }
+
+            if ($request->hasFile('file_peserta')) {
+                try {
+                    $file = $request->file('file_peserta');
+                    $filename = time() . '_peserta.' . $file->getClientOriginalExtension();
+                    
+                    if (\Illuminate\Support\Facades\Storage::disk('google')->put($filename, file_get_contents($file))) {
+                        $adapter = \Illuminate\Support\Facades\Storage::disk('google')->getAdapter();
+                        $metadata = $adapter->getMetadata($filename);
+                        $filePeserta = 'https://drive.google.com/uc?id=' . $metadata['path'];
+                    }
+                } catch (\Exception $e) {
+                    \Illuminate\Support\Facades\Log::error('Upload file peserta error: ' . $e->getMessage());
+                }
+            }
+
+            AbsensiPegawai::create([
+                'user_id' => auth()->id(),
+                'nama_pemantau' => auth()->user()->name,
+                'jenis_tutorial' => $request->jenis_tutorial,
+                'tanggal' => $request->tanggal,
+                'jam_tutorial' => $request->jam_tutorial,
+                'pertemuan_ke' => $request->pertemuan_ke,
+                'kode_nama_matkul_kelas' => $request->kode_nama_matkul_kelas,
+                'id_kelas_tutorial' => $request->id_kelas_tutorial,
+                'id_tutor' => $request->id_tutor,
+                'nama_tutor' => $request->nama_tutor,
+                'tgl_jam_mulai_pantau' => $request->tgl_jam_mulai_pantau,
+                'jml_mhs_seharusnya' => $request->jml_mhs_seharusnya,
+                'jml_mhs_hadir' => $request->jml_mhs_hadir,
+                'jenis_pemantauan' => $request->jenis_pemantauan,
+                'kbm_absensi' => $request->kbm_absensi,
+                'kbm_materi' => $request->kbm_materi,
+                'kbm_media' => $request->kbm_media,
+                'kbm_diskusi' => $request->kbm_diskusi,
+                'kbm_pengarahan' => $request->kbm_pengarahan,
+                'bahas_tugas' => $request->bahas_tugas,
+                'jam_akhir_pantau' => $request->jam_akhir_pantau,
+                'praktik_baik' => $request->praktik_baik,
+                'temuan_ketidaksesuaian' => $request->temuan_ketidaksesuaian,
+                'kesan_pembelajaran' => $request->kesan_pembelajaran,
+                'kendala_tutorial' => $request->kendala_tutorial,
+                'saran_perbaikan' => $request->saran_perbaikan,
+                'file_materi' => $fileMateri,
+                'file_peserta' => $filePeserta,
+                'link_video' => $request->link_video,
+                'latitude' => $request->latitude,
+                'longitude' => $request->longitude,
+            ]);
+
+            return redirect()->back()->with('success', 'Data pemantauan berhasil disimpan!');
+        } catch (\Exception $e) {
+            \Illuminate\Support\Facades\Log::error('Absensi store error: ' . $e->getMessage());
+            return redirect()->back()->with('error', 'Terjadi kesalahan: ' . $e->getMessage())->withInput();
         }
+    }
 
         $filePeserta = null;
         if ($request->hasFile('file_peserta')) {
